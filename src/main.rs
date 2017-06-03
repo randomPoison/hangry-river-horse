af1bed1dafa2cdc0bce13d1e4962b7dd83ac5046
David LeGare <excaliburhissheath@gmail.com>
2017-06-03 14:54:08 -0500

    Setup index.html and have it looks for server events

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,18 @@ fn main() {
         "Got display request"
     });
 
-    server.mount("/", middleware! { |_request, response|
-        "Got client request"
+    server.mount("/events/", middleware! { |_request, response|
+        "Here are some events"
     });
 
+    server.mount("/", StaticFilesHandler::new("www/"));
+
+    server.utilize(logger_fn);
+
     server.listen("127.0.0.1:6767").unwrap();
+}
+
+fn logger_fn<'mw>(req: &mut Request, res: Response<'mw>) -> MiddlewareResult<'mw> {
+    println!("logging request from logger fn: {:?}", req.origin.uri);
+    res.next_middleware()
 }
