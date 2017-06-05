b7a31fee9c3a994918ddebe41c7ad54e266ccd23
David LeGare <excaliburhissheath@gmail.com>
2017-06-04 20:44:17 -0500

    Add separate endpoints for client and host

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,50 @@ fn main() {
 
     rouille::start_server("localhost:6767", move |request| {
         router!(request,
-            (GET) (/api/display) => {
-                let (response, websocket) = try_or_400!(websocket::start(&request, Some("echo")));
+            (GET) (/api/host) => {
+                let (response, websocket) = try_or_400!(websocket::start::<String>(&request, None));
 
                 // Because of the nature of I/O in Rust, we need to spawn a separate thread for
                 // each websocket.
                 thread::spawn(move || {
                     // This line will block until the `response` above has been returned.
                     let mut websocket = websocket.recv().unwrap();
+
+                    websocket.send_text("Here's some straight up garbo").unwrap();
+
+                    loop {
+                        // We wait for a new message to come from the websocket.
+                        let message = match websocket.next() {
+                            Some(m) => m,
+                            None => break,
+                        };
+
+                        match message {
+                            websocket::Message::Text(txt) => {
+                                // If the message is text, send it back with `send_text`.
+                                println!("received {:?} from a websocket", txt);
+                                websocket.send_text(&txt).unwrap();
+                            },
+                            websocket::Message::Binary(_) => {
+                                println!("received binary from a websocket");
+                            },
+                        }
+                    }
+                });
+
+                response
+            },
+
+            (GET) (/api/client) => {
+                let (response, websocket) = try_or_400!(websocket::start::<String>(&request, None));
+
+                // Because of the nature of I/O in Rust, we need to spawn a separate thread for
+                // each websocket.
+                thread::spawn(move || {
+                    // This line will block until the `response` above has been returned.
+                    let mut websocket = websocket.recv().unwrap();
+
+                    websocket.send_text("Here's some straight up garbo").unwrap();
 
                     loop {
                         // We wait for a new message to come from the websocket.
---
+++ www/client.js
@@ -0,0 +1,24 @@
+// Initialize WebSocket connetion without waiting for the DOM to
+// be ready. I don't know if that's actually a good idea, but
+// whatevs.
+let socket = new WebSocket('ws://localhost:6767/api/client');
+socket.onmessage = function(event) {
+    console.log('socket event: ', event);
+};
+
+// Callback for "Feed Me" button. Sends a message to the backend notifying that
+// a hippo has been fed.
+function feedMe() {
+    socket.send(JSON.stringify({
+        event: 'feed-me',
+        amount: 1,
+    }));
+}
+
+// Initialize Vue.js with some info I guess.
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
