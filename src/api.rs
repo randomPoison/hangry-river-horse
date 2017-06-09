c937183cd9b5756992b12289f8a4399cc9e86071
David LeGare <david.legare@scientificgames.com>
2017-06-09 14:01:35 -0500

    Don't panic on invalid player ID

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,15 @@
 use broadcast::*;
 use game::*;
+use rocket::http::Status;
+use rocket::response::*;
 use rocket::State;
 use rocket_contrib::JSON;
 use std::sync::*;
 
+/// The response sent back from the `/register-player` endpoint.
 #[derive(Debug, Serialize)]
 pub struct RegisterPlayerResponse {
+    /// The `PlayerId` that was generated for the new player.
     id: PlayerId,
 }
 
@@ -36,22 +40,31 @@ pub fn register_player(
     })
 }
 
+/// The request expected from the client for the `/feed-me` endpoint.
 #[derive(Debug, Deserialize)]
 pub struct FeedPlayerRequest {
+    /// The `PlayerId` for the player that clicked their "Feed Me" button.
     player: PlayerId,
 }
 
+/// The response sent back from the `/feed-me` endpoint.
 #[derive(Debug, Serialize)]
 pub struct FeedPlayerResponse {
     score: usize,
 }
 
+/// Feeds a player's hippo, increasing the player's score.
+///
+/// # Errors
+///
+/// If the `player` member of `payload` isn't a valid `PlayerId` (i.e. the ID isn't in `scores`),
+/// Then `Err(InvalidPlayer)` is returned.
 #[post("/feed-me", format = "application/json", data = "<payload>")]
 pub fn feed_player(
     payload: JSON<FeedPlayerRequest>,
     scoreboard: State<Mutex<Scoreboard>>,
     broadcaster: State<HostBroadcaster>,
-) -> JSON<FeedPlayerResponse>
+) -> Result<JSON<FeedPlayerResponse>>
 {
     let payload = payload.into_inner();
     let player_id = payload.player;
@@ -60,11 +73,11 @@ pub fn feed_player(
     let score = {
         let mut scoreboard = scoreboard.lock().expect("Scoreboard mutex was poisoned");
 
-        // TODO: Return a proper error code if the player isn't registered, and also maybe don't
-        // poison the mutex while we're at it.
+        // Get the player's current score, or return an `InvalidPlayer` error if it's not in
+        // the scoreboard.
         let score = scoreboard
             .get_mut(&player_id)
-            .expect("Tried to add score to a nonexistent player");
+            .ok_or(Error::InvalidPlayer(player_id))?;
         *score += 1;
         *score
     };
@@ -76,7 +89,28 @@ pub fn feed_player(
     });
 
     // Respond to the client.
-    JSON(FeedPlayerResponse {
+    Ok(JSON(FeedPlayerResponse {
         score: score,
-    })
+    }))
 }
+
+/// The error type for an API requests that can fail.
+#[derive(Debug, Serialize)]
+pub enum Error {
+    /// Indicates that an invalid player was specified for the operation.
+    ///
+    /// This might occur if the client code cached the player ID from a previous session, and is
+    /// now trying to use the ID in a session where it is no longer valid. Re-registering the
+    /// player to generate a new ID should fix the issue.
+    InvalidPlayer(PlayerId),
+}
+
+impl<'r> Responder<'r> for Error {
+    fn respond(self) -> ::std::result::Result<Response<'r>, Status> {
+        use rocket::response::status::Custom;
+
+        Custom(Status::BadRequest, JSON(self)).respond()
+    }
+}
+
+pub type Result<T> = ::std::result::Result<T, Error>;
--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,28 @@ mod api;
 mod broadcast;
 mod game;
 
+/// Routes `/` to the player landing page.
+///
+/// Serves `www/client.html` when the player navigates directly to `localhost:6767`, that way people
+/// don't have to manually go to `localhost:6767/client.html`.
 #[get("/")]
 fn static_serve_player() -> io::Result<NamedFile> {
     NamedFile::open(Path::new("www/client.html"))
 }
 
-#[get("/display")]
+/// Routes `/host` to the host landing page.
+///
+/// Server `www/host.html` when navigating to to `localhost:6767/host`, that way people don't have
+/// to manually add the `.html` to the end.
+#[get("/host")]
 fn static_serve_display() -> io::Result<NamedFile> {
     NamedFile::open(Path::new("www/host.html"))
 }
 
-/// Serves files from the `www/` directory.
+/// Fallback static file server route.
+///
+/// Any requests that aren't matched against an API route and aren't the special case `/` and `/host`
+/// routes will be served as static files, returning a 404 error if the file doesn't exist.
 #[get("/<file..>")]
 fn static_serve(file: PathBuf) -> io::Result<NamedFile> {
     NamedFile::open(Path::new("www/").join(file))
@@ -44,8 +55,15 @@ fn main() {
 
     // Start the main Rocket application.
     rocket::ignite()
-        .mount("/", routes![static_serve, static_serve_player, static_serve_display])
-        .mount("/api", routes![api::register_player, api::feed_player])
+        .mount("/", routes![
+            static_serve,
+            static_serve_player,
+            static_serve_display,
+        ])
+        .mount("/api", routes![
+            api::register_player,
+            api::feed_player,
+        ])
         .manage(PlayerIdGenerator::new())
         .manage(host_broadcaster)
         .manage(client_broadcaster)
