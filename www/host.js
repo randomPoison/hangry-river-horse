7e116bde55623abd2e07a239cdb48348f9b37da9
David LeGare <excaliburhissheath@gmail.com>
2017-06-20 08:59:39 -0500

    Implement hippo heads with Vue.js (#24)

--- www/host.js
+++ www/host.js
@@ -1,44 +1,47 @@
 'use strict';
 
-// Initialize the VueJS app. This is used for debug rendering.
-let debug = new Vue({
+const TOP_SIDE = 0;
+const RIGHT_SIDE = 1;
+const BOTTOM_SIDE = 2;
+const LEFT_SIDE = 3;
+
+// Initialize the VueJS app. This is used for app rendering.
+let app = new Vue({
     el: '#vue-root',
     data: {
-        isDebugEnabled: true,
-        players: [],
+        // Track which hippos should be on each side of the screen.
+        topHippos: [],
+        rightHippos: [],
+        bottomHippos: [],
+        leftHippos: [],
+
+        // Keep a map to allow us ot lookup hippos by player ID. The hippos keep a reference to
+        // their player so this also allows us to lookup players by ID.
+        hippoMap: {},
     },
 });
 
-// Initialize the Phaser game. Phaser provides most of the functionality that
-// we use to build the display.
-let game = new Phaser.Game({
-    width: 800,
-    height: 600,
-    parent: 'phaser-root',
-    state: {
-        preload: preload,
-        create: create,
-    },
+Vue.component('hippo-head', {
+    props: ['name', 'score', 'id'],
+    template: `
+    <div class="hippo-head">
+        <div class="hippo-text">
+            <div class="hippo-name">{{ name }}</div>
+            <div class="hippo-score">Score: {{ score }}</div>
+        </div>
+        <img src="assets/hippo.jpg" class="hippo-head-image" :id="id">
+    </div>
+    `,
 });
 
-let players = [];
-
-function preload() {
-    game.load.image('hippo', 'assets/hippo.jpg');
-    game.load.image('background', 'assets/backdrop.jpg');
-}
-
-function create() {
-    // Create a sprite for the backdrop image and have it fill the canvas.
-    let background = game.add.sprite(0, 0, 'background');
-    background.width = 800;
-    background.height = 600;
-
-    // Create a sprite for the hippo head and make is relatively small.
-    let sprite = game.add.sprite(80, 0, 'hippo');
-    sprite.width = 100;
-    sprite.height = 100;
-}
+// Helpers to allow us to place hippos in clockwise order.
+let sides = [
+    app.topHippos,
+    app.rightHippos,
+    app.bottomHippos,
+    app.leftHippos,
+];
+let currentSide = 0;
 
 // Setup a websocket to listen for updates from the server.
 let socket = new WebSocket('ws://' + window.location.hostname + ':6769');
@@ -47,28 +50,89 @@ socket.onmessage = (event) => {
     let payload = JSON.parse(event.data);
 
     if (payload['PlayerRegistered']) {
-        let player = payload['PlayerRegistered'];
-        players.push(player);
+        registerPlayer(payload['PlayerRegistered']);
     } else if (payload['PlayerScore']) {
         let info = payload['PlayerScore'];
 
-        // Find the existing score object in `debug.scores`. If the player is already in the list,
-        // then update their score, otherwise add them to the list.
-        // TODO: We probaly shouldn't try to track score for players that haven't been registered,
-        // since we wouldn't know there username.
-        let existing_score = players.find(player => player.id === info.id);
-        assert(existing_score != null, 'Got a score for an unregistered player: ' + info.player);
-        existing_score.score = info.score;
+        // Find the hippo/player for the player that scored.
+        let hippo = app.hippoMap[info.id];
+        assert(hippo != null, 'Unable to find hippo for ID: ' + info.id);
+
+        // Updated the local score for the player.
+        hippo.player.score = info.score;
+
+        // Animate the hippo head to match the score increase. The direction of the chomp animation
+        // depends on the side of the screen that the hippo is on.
+        let element = document.getElementById(hippo.player.id);
+        switch (hippo.side) {
+            case TOP_SIDE: {
+                TweenMax.fromTo(
+                    element,
+                    .2,
+                    { top: 0 },
+                    { top: '100px', repeat: 1, yoyo: true, overwrite: 'none' },
+                );
+            } break;
+
+            case RIGHT_SIDE: {
+                TweenMax.fromTo(
+                    element,
+                    .2,
+                    { right: 0 },
+                    { right: '100px', repeat: 1, yoyo: true, overwrite: 'none' },
+                );
+            } break;
+
+            case BOTTOM_SIDE: {
+                TweenMax.fromTo(
+                    element,
+                    .2,
+                    { bottom: 0 },
+                    { bottom: '100px', repeat: 1, yoyo: true, overwrite: 'none' },
+                );
+            } break;
+
+            case LEFT_SIDE: {
+                TweenMax.fromTo(
+                    element,
+                    .2,
+                    { left: 0 },
+                    { left: '100px', repeat: 1, yoyo: true, overwrite: 'none' },
+                );
+            } break;
+
+            default: throw new Error('Unrecognized hippo side: ' + hippo.side);
+        }
     }
 };
 
 // When we first boot up we need to get the current list of players.
 get('/api/players', response => {
-    players = response['players'];
+    let players = response['players'];
+    assert(players != null, '/api/players response was missing a "players" member');
 
-    // Set the `debug.players` to be `players`, that way any changes to
-    // `players` will automatically be reflected in the debug information.
-    debug.players = players;
-
-    // TODO: Create a hippo for each player.
+    // Add players to the player map, so we can find them by ID.
+    for (let player of players) {
+        registerPlayer(player);
+    }
 });
+
+/**
+ * Creates a hippo for the new player and adds it to one side of the screen.
+ */
+function registerPlayer(player) {
+    // Create a hippo object for the player.
+    let hippo = {
+        player: player,
+        side: currentSide,
+    };
+
+    // Add the hippo to the hippo map.
+    assert(app.hippoMap[player.id] == null, 'Hippo already exists for ID: ' + player.id);
+    app.hippoMap[player.id] = hippo;
+
+    // Add the hippo to one of the sides of the screen. We cycle the sides clockwise to make sure
+    // the hippos are distributed evenly.
+    sides[currentSide].push(hippo);
+    currentSide = (currentSide + 1) % 4;
+}
